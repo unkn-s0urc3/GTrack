c0905390830560ff0060bc31001e62fc93c96d74
Unknown Source <165537535+unkn-s0urc3@users.noreply.github.com>
2025-05-20 17:50:21 +0700

    Add Prism for MVVM. Create MainView. Configure project to use Prism (GTrack-Control)

--- GTrack-Control/App.xaml.cs
+++ GTrack-Control/App.xaml.cs
@@ -1,7 +1,17 @@
 ﻿using System.Windows;
+using GTrack_Control.Views;
 
 namespace GTrack_Control;
 
-public partial class App : Application
+public partial class App : PrismApplication
 {
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        
+    }
+
+    protected override Window CreateShell()
+    {
+        return ContainerLocator.Container.Resolve<MainView>();
+    }
 }
---
+++ GTrack-Control/Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace GTrack_Control.Views;
+
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
